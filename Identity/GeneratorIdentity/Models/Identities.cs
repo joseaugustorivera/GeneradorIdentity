dff338d4a2c2d5a576138a957d01ad8f118b6d6d
Jose Augusto Rivera <a2017115231@estudiantes.upsa.edu.bo>
2021-03-19 23:37:24 -0400

    Ejercicio completo.

---
+++ Identity/GeneratorIdentity/Models/Identities.cs
@@ -0,0 +1,10 @@
+﻿namespace GeneratorIdentity.Models
+{
+    public class Identity
+    {
+        public int IdentityID { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public string Sex { get; set; }
+    }
+}
